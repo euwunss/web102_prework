2af8c5dc964e29f2135ed0bc799698cd1a7d6b5d
euwunss <olhalashchukevych@gmail.com>
2025-01-26 19:41:37 -0600

    Add info about company at the top

--- index.js
+++ index.js
@@ -140,10 +140,21 @@ allBtn.addEventListener("click", showAllGames);
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
+const numUnfundedGames = GAMES_JSON.filter(
+  (game) => game.pledged < game.goal
+).length;
 
 // create a string that explains the number of unfunded games using the ternary operator
+const descriptionStr = `A total of $${totalRaised.toLocaleString(
+  "en-US"
+)} has been raised for ${gamesNumber} games. Currently, ${numUnfundedGames} ${
+  numUnfundedGames == 1 ? "game remains" : "games remain"
+} unfunded. We need your help to fund these amazing games!`;
 
 // create a new DOM element containing the template string and append it to the description container
+const descriptionPar = document.createElement("p");
+descriptionPar.innerHTML = descriptionStr;
+descriptionContainer.appendChild(descriptionPar);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
